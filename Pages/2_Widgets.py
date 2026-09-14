1b84ec6acfd2146270b9f0f678472cfa5f6d35f0
ShivtejPatil2807 <shivtejpati2807@gmail.com>
2026-09-14 21:40:26 +0530

    Widgets

--- Pages/2_Widgets.py
+++ Pages/2_Widgets.py
@@ -1,41 +1,150 @@
-# import streamlit as st
+import streamlit as st
 
-# st.set_page_config(
-#     page_title="Widgets",
-#     page_icon="🎛️"
-# )
+st.set_page_config(
+    page_title="Widgets",
+    page_icon="🎛️"
+)
 
-# st.title("🎛️ Streamlit Widgets")
-
-# # --------------------------------------------------
-# # 1. st.button()
-# # --------------------------------------------------
-
-# st.header("1. st.button()")
-
-# st.write(
-#     "Creates a button that users can click to perform an action."
-# )
-
-# st.subheader("Syntax")
-
-# st.code(
-#     'st.button("Click Me")',
-#     language="python"
-# )
-
-# st.subheader("Example")
-
-# if st.button("Click Me"):
-#     st.success("Button clicked! 🎉")
+st.title("🎛️ Streamlit Widgets")
 
 
-# # --------------------------------------------------
-# # 2. st.checkbox()
-# # --------------------------------------------------
+st.header("1. st.button()")
+st.write("Creates a button that users can click to perform an action.")
 
-# st.header("st.checkbox()")
+st.code('st.button("Click Me")', language = "python")
+if st.button("Click Me"):
+    st.success("Button clicked! 🎉")
 
-# st.write(
-#     "Creates a button that users can click to perform an action."
-# )
+
+st.header("2. st.checkbox()")
+st.write("Creates a box that users can tick or untick.")
+
+st.code('st.checkbox("Check the box")', language = "python")
+st.checkbox("IF YOU AGREE")
+
+
+st.header("3. st.radio()")
+st.write("Lets the user choose one option from several options.")
+
+st.code('st.radio("Select one option")', language = "python")
+lan = st.radio("Which language is best for programminng ?",["HTML","JAVA","PYTHON","C++"])
+st.write(f"You Selected : {lan}")
+
+
+st.header("4. st.selectbox()")
+st.write("Creates a dropdown menu where the user chooses one option.")
+
+st.code('st.selectbox()', language = "python")
+sel = st.selectbox("how would you like to be contacted ?",["Email","Home Phone","Mobile Phone","Twitter"])
+st.write(f"You Selected : {sel}")
+
+
+st.header("5. st.multiselect()")
+st.write("Lets the user choose multiple options from a list.")
+
+st.code('st.multiselect()', language = "python")
+st.multiselect("Who is the creator of Python ?",["Guido Van Rossum","Dennie Ratchie","Bjarne Strustrup","Jamps Gosling"])
+
+
+st.header("6. st.select_slider()")
+st.write("Lets the user select one option by sliding through choices.")
+
+st.code('st.select_slider()',language = "python")
+st.select_slider("Increse Volummn 50%",["10","20","30","40","50","60","70","80","90","100"])
+
+
+st.header("7. st.text_input()")
+st.write("Creates a box where the user can type a short piece of text.")
+
+st.code('st.text_input()', language = "python")
+st.text_input("Enter Your Age :")
+
+
+st.header("8. st.text_area()")
+st.write("Creates a larger box where the user can type longer text.")
+
+st.code('st.text_area()', language = "python")
+st.text_area(label ="Enter your text below:", placeholder ="Type something amazing...")
+
+
+st.header("9. st.number_input()")
+st.write("Creates an input box where the user can enter a number.")
+
+st.code('st.number_input()', language = "python")
+st.number_input(" Enter any number")
+
+
+st.header("10. st.slider()")
+st.write("Lets the user choose a number by moving a slider.")
+
+st.code('st.slider()', language = "python")
+age = st.slider(
+    label="Select your age",
+    min_value=0,
+    max_value=100,
+    value=25, 
+    step=1    
+)
+st.write(f"You are {age} years old.")
+
+
+st.header("11. st.date_input()")
+st.write("Lets the user select a date from a calendar.")
+
+st.code('st.date_input()', language = "python")
+st.date_input("Calender")
+
+
+st.header("12 .st.time_input()")
+st.write("Lets the user select a time.")
+
+st.code('st.time_input()', language = "python")
+st.time_input("Clock:")
+
+
+st.header("13. st.file_uploader()")
+st.write("Lets the user upload a file to the Streamlit app.")
+
+st.code('st.file_uploader()', language = "python")
+st.file_uploader("Upload you document file:")
+
+
+st.header("14. st.camera_input()")
+st.write("Lets the user take/upload a photo using their camera.")
+
+st.code('st.camera_input()', language = "python")
+st.camera_input("Camera:")
+
+
+st.header("15. st.color_picker()")
+st.write("Lets the user choose a color.")
+
+st.code('st.color_picker()', language = "python")
+st.color_picker("Pick any color")
+
+
+st.header("16. st.download_button()")
+st.write("Creates a button that lets the user download something.")
+
+data = """
+Hello Everyone✋!
+
+This is a file created using Streamlit.
+You can download this file to your computer.
+"""
+
+st.code('st.download_button()', language = "python")
+st.download_button(
+    label="Download file",
+     data= data,
+    file_name="filename.ext",
+    mime="mime/type"
+)
+
+
+st.header("17. st.link_button()")
+st.write("Creates a button that takes the user to another webpage/link.")
+
+
+st.code('st.link_button()', language = "python")
+st.link_button("Go to Streamlit", "https://streamlit.io")
